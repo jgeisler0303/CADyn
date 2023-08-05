bb3d6c7b0ed19d322229016a567eb1626215329e
Jens Geisler <jens.geisler@hs-flensburg.de>
2023-08-05 10:24:04 +0200

    improved gen/makeCAGEM.m

--- gen/makeCAGEM.m
+++ gen/makeCAGEM.m
@@ -1,4 +1,4 @@
-function makeCAGEM(model_path, target_path, skip_gen)
+function makeCAGEM(model_path, target_path, skip_gen, files_to_generate)
 maxima_path= getenv('maxima_path');
 if isempty(maxima_path)
     error('Please set path to maxima program via "setenv(''maxima_path'', ''PATH_TO_MAXIMA.EXE'');');
@@ -7,6 +7,13 @@ end
 cagem_path= getenv('cagem_path');
 if isempty(cagem_path)
     error('Please set path to cagem.mac script via "setenv(''cagem_path'', ''PATH_TO_CAMGEM.MAC'');');
+end
+
+if ~exist('files_to_generate', 'var')
+    files_to_generate= {'cpp_direct'};
+end
+if ~iscell(files_to_generate)
+    files_to_generate= {files_to_generate};
 end
 
 cagem_base= fileparts(cagem_path);
@@ -21,8 +28,9 @@ if ~exist(target_path, 'dir')
     mkdir(target_path);
 end
 
+% TODO: check for file to be generated according to files_to_generate
 code_file= fullfile(target_path, [model_name '_direct.hpp']);
-if ~exist('skip_gen', 'var')
+if ~exist('skip_gen', 'var') || isempty(skip_gen)
     skip_gen= false;
     if exist(code_file, 'file')
         dd_src= dir(model_path);
@@ -47,19 +55,20 @@ if ~skip_gen
         '--batch-string="', ...
         'load(\"' cagem_path '\")\$ ', ...
         'sys:cagem(\"' model_path '\")\$ ', ...
-        'load(\"' strrep(cagem_path, 'cagem.mac', 'cagem_gen_cpp_direct.mac') '\")\$ ', ...
-        'cagem_gen_cpp_direct(sys, \"' target_path '\")\$ ', ...
         'load(\"' strrep(cagem_path, 'cagem.mac', 'cagem_gen_matlab.mac') '\")\$ ', ...
         'writeMATLAB_nonlinear(sys,\"' target_path '/' model_name '_matlab_nonlin.m' '\")\$ ', ...
         'writeMATLAB_linear(sys,\"' target_path '/' model_name '_matlab_lin.m' '\")\$ ', ...
-        'load(\"' strrep(cagem_path, 'cagem.mac', 'cagem_gen_acados.mac') '\")\$ ', ...
-        'cagem_acados(sys,\"' target_path '\")\$ ', ...
-        'load(\"' strrep(cagem_path, 'cagem.mac', 'cagem_gen_acados_py.mac') '\")\$ ', ...
-        'cagem_acados_py(sys,\"' target_path '\")\$ ', ...
         'load(\"' strrep(cagem_path, 'cagem.mac', 'cagem_gen_indices.mac') '\")\$ ', ...
         'cagem_indices(sys,\"' target_path '\")\$ ', ...
-        '"'];
-        
+        ];
+    for i= 1:length(files_to_generate)
+        command_str= [command_str, ...
+        'load(\"' strrep(cagem_path, 'cagem.mac', ['cagem_gen_' files_to_generate{i} '.mac']) '\")\$ ', ...
+        'cagem_gen_' files_to_generate{i} '(sys, \"' target_path '\")\$ ', ...
+        ];
+    end
+    command_str= [command_str '"'];
+
     fprintf('%s\n', command_str);
         
     [status, res]= system(command_str);
